0a31243b4a42d396fb557fcff1626d2291ad23fa
ASUS <ASUS@DESKTOP-TRT6E7B>
2023-04-06 13:40:31 +0200

    Écriture d'un EJB session stateless GestionnaireCompte et déclaration de la source de données

---
+++ src/main/java/mg/itu/randrianarivelo/ejb/GestionnaireCompte.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.randrianarivelo.ejb;
+
+import jakarta.ejb.Stateless;
+
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",
+    password="12345678", 
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+     "allowPublicKeyRetrieval=true"
+    }
+)   
+@Stateless
+public class GestionnaireCompte {
+    
+}
