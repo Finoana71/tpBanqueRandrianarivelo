9107f20fe0dfb3e7ca7ed482be130ce6bc66bfad
Finoana71 <finoanamandresy71@gmail.com>
2023-04-11 16:43:47 +0300

    JSF pour déposer ou retirer de l'argent sur un compte

---
+++ src/main/java/mg/itu/randrianarivelo/jsf/MouvementBean.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.randrianarivelo.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author user050
+ */
+@Named(value = "mouvementBean")
+@ViewScoped
+public class MouvementBean implements Serializable {
+
+    /**
+     * Creates a new instance of MouvementBean
+     */
+    public MouvementBean() {
+    }
+    
+}
