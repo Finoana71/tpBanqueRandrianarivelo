62c38527ecc15abbbe64c906b7c1b6d1f5ec8090
Finoana71 <finoanamandresy71@gmail.com>
2023-04-11 10:25:43 +0300

    Page qui liste tous les comptes(backing bean et jsf)

---
+++ src/main/java/mg/itu/randrianarivelo/jsf/ListeComptes.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.randrianarivelo.jsf;
+
+import jakarta.ejb.EJB;
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.randrianarivelo.ejb.GestionnaireCompte;
+import mg.itu.randrianarivelo.entities.CompteBancaire;
+
+/**
+ *
+ * @author user050
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+
+    @EJB
+    GestionnaireCompte gest;
+    
+    private List<CompteBancaire> allComptes;
+    
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+    
+    public List<CompteBancaire> getAllComptes(){
+        if(allComptes == null){
+            allComptes = gest.getAllComptes();
+        }
+        return allComptes;
+    }
+}
