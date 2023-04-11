1b7459687cdf6422ed24db1a75844af7c3fe7ff8
Finoana71 <finoanamandresy71@gmail.com>
2023-04-11 10:40:19 +0300

    Page index.xhtml et test de l'application, avec correction(entité, jsf)

--- src/main/java/mg/itu/randrianarivelo/ejb/GestionnaireCompte.java
+++ src/main/java/mg/itu/randrianarivelo/ejb/GestionnaireCompte.java
@@ -8,7 +8,6 @@ import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 import java.util.List;
 import mg.itu.randrianarivelo.entities.CompteBancaire;
